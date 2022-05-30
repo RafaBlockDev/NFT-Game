2de29554c1bc8cd2a2215d2bcbb747379495b1e1
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-05-29 21:22:19 -0500

    Initializing NFT mint and constructor

--- contract/contracts/RafaGame.sol
+++ contract/contracts/RafaGame.sol
@@ -5,7 +5,47 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract NFTGame {
-    constructor() {
-        console.log("Rafa NFT Game");
+    /// @dev The game will hold attributes in a struct
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string description;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+    /// @dev A lil array to help the user hold the default data for our characters.
+    /// @notice This will be helpful when the user mint new characters and need to know
+    // things like their HP, AD, etc.
+    CharacterAttributes[] defaultCharacters;
+
+    /// @dev Data passed in to the contract when it is first created initializing the characters
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDmg
+    ) {
+        for (uint256 i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log(
+                "Done initializing %s w/ HP %s, img %s ",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+        }
     }
 }
